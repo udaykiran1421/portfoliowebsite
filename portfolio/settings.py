7f8ff5732aed04fffe6a3a713348f581c47afbe0
udaykiran1421 <udaykiranbudi@gmail.com>
2021-07-20 00:27:39 +0530

    commit on master

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@2h1$+4kf0^hhxr9js+%_cc^w!vmg6tp===#y!(@d-l(u6@$a4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolio-uday.herokuapp.com','127.0.0.1']
 
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
